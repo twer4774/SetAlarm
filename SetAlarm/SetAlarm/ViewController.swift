66b94cf62f35e09275c511032ee3434ab9936ff4
twer4774 <twer4774@gmail.com>
2018-10-01 12:11:09 +0900

    create: Init project

---
+++ SetAlarm/SetAlarm/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SetAlarm
+//
+//  Created by WonIk on 2018. 10. 1..
+//  Copyright © 2018년 Walter. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
